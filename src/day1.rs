0ae738a9bf63fe40572afbcc545338d2aaf35982
Tim T. <tim@mw3.ch>
2024-12-01 20:50:48 +0100

    rename list1, list2 to list_left, list_right

--- src/day1.rs
+++ src/day1.rs
@@ -2,48 +2,48 @@
 
 #[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> (Vec<u32>, Vec<u32>) {
-    let mut list1 = Vec::new();
-    let mut list2 = Vec::new();
+    let mut list_left = Vec::new();
+    let mut list_right = Vec::new();
 
     let parse = |s: &str| s.trim().parse::<u32>().unwrap();
     for line in input.lines() {
         let (left, right) = line.split_once(' ').unwrap();
 
-        list1.push(parse(left));
-        list2.push(parse(right));
+        list_left.push(parse(left));
+        list_right.push(parse(right));
     }
 
-    (list1, list2)
+    (list_left, list_right)
 }
 
 #[aoc(day1, part1)]
 pub fn get_total_distance(input: &(Vec<u32>, Vec<u32>)) -> u32 {
-    let mut list1 = input.0.clone();
-    let mut list2 = input.1.clone();
+    let mut list_left = input.0.clone();
+    let mut list_right = input.1.clone();
 
-    list1.sort_unstable();
-    list2.sort_unstable();
+    list_left.sort_unstable();
+    list_right.sort_unstable();
 
-    list1
+    list_left
         .iter()
-        .zip(list2)
+        .zip(list_right)
         .fold(0, |acc, (a, b)| acc + b.abs_diff(*a))
 }
 
 #[aoc(day1, part2)]
 pub fn get_similarity_score(input: &(Vec<u32>, Vec<u32>)) -> u32 {
-    let list1 = &input.0;
-    let list2 = &input.1;
+    let list_left = &input.0;
+    let list_right = &input.1;
 
-    let max_val = list2.iter().max().unwrap_or(&0);
+    let max_val = list_right.iter().max().unwrap_or(&0);
 
     let mut map: Vec<u32> = vec![0; (max_val + 1) as usize];
 
-    for item in list2 {
+    for item in list_right {
         map[*item as usize] += 1;
     }
 
-    list1.iter().fold(0, |acc, item| {
+    list_left.iter().fold(0, |acc, item| {
         acc + if item <= max_val {
             map[*item as usize] * item
         } else {
